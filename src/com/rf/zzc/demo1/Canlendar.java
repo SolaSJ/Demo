972f0d0d97be31f103722290324352d9d363b704
zhouzhichen <550956150@qq.com>
2017-08-24 15:47:40 +0800

    1

--- src/com/rf/zzc/demo1/Canlendar.java
+++ src/com/rf/zzc/demo1/Canlendar.java
@@ -5,14 +5,14 @@ import java.util.Calendar;
 public class Canlendar {
 
 	public static void main(String[] args) {
-		month(2000,2);
+		month(2000, 2);
 	}
-	public static void month(int a,int b){
-		Calendar c=Calendar.getInstance();
-		 c.set(a, b, 1);
+
+	public static void month(int a, int b) {
+		Calendar c = Calendar.getInstance();
+		c.set(a, b, 1);
 		c.add(Calendar.DAY_OF_MONTH, -1);
-		
-		
-		System.out.println(c.get(Calendar.DAY_OF_MONTH)) ;
+
+		System.out.println(c.get(Calendar.DAY_OF_MONTH));
 	}
 }
